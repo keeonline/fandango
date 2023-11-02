de49cc31b55a3a6fe9137731df95da17aee603c6
keeonline <keeonline@outlook.com>
2023-11-02 16:35:04 +0000

    wip

--- src/main/java/com/keeonline/fandango/mip/MipConnection.java
+++ src/main/java/com/keeonline/fandango/mip/MipConnection.java
@@ -1,17 +1,19 @@
 package com.keeonline.fandango.mip;
 
-import java.net.*;
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.nio.ByteBuffer;
 import java.time.LocalTime;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.client.RestTemplate;
 
-import com.keeonline.fandango.api.model.MessageBytesDto;
 import com.keeonline.fandango.api.model.MessageDto;
 import com.keeonline.fandango.api.service.McbnService;
 import com.keeonline.fandango.iso8583.field.transformer.exception.FieldTransformerException;
@@ -23,9 +25,7 @@ public class MipConnection extends Thread {
 
     private static boolean isConnected = false;
 
-    private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
+"{\"messageTypeIdentifier\":\"0100\",\"primaryAccountNumber\":5039990000100006,\"processingCode\":{\"transactionType\":0,\"accountTypeFrom\":0,\"accountTypeTo\":0},\"amountTransaction\":{\"amount\":10,\"currency\":\"GBP\"},\"amountCardholderBilling\":{\"amount\":10,\"currency\":\"GBP\"},\"transmissionDateAndTime\":{\"date\":{\"year\":2023,\"month\":10,\"day\":30},\"time\":{\"hour\":15,\"minute\":15,\"second\":43,\"nano\":0}},\"conversionRateCardholderBilling\":1.000000,\"systemTraceAuditNumber\":9,\"timeLocalTransaction\":{\"hour\":15,\"minute\":11,\"second\":22,\"nano\":0},\"dateLocalTransaction\":{\"year\":2023,\"month\":10,\"day\":31},\"dateExpiration\":{\"year\":2023,\"month\":10,\"day\":29},\"dateSettlement\":{\"year\":2023,\"month\":10,\"day\":31},\"dateConversion\":{\"year\":2023,\"month\":10,\"day\":31},\"merchantType\":5311,\"posEntryModeCode\":{\"panEntryMode\":1,\"pinEntryMode\":0},\"acquiringInstitutionIdentificationCode\":130000,\"retrievalReferenceNumber\":\"000000000001\",\"cardAcceptorTerminalIdentification\":\"T0000000\",\"cardAcceptorIdentificationCode\":\"A000000\",\"cardAcceptorNameLocation\":{\"cardAcceptorOrAtmLocation\":\"A STORE 0000\",\"cityName\":\"LONDON\",\"countryCode\":\"GBP\"},\"additionalDataPrivateUse\":\"R9203721\",\"pointOfServiceData\":\"0001000000700826E1 6AN\",\"privateUseFields\":\"MCC000001\"}";
 
     public static boolean isConnected() {
         return isConnected;
@@ -38,6 +38,7 @@ public class MipConnection extends Thread {
     public void run() {
         Socket socket;
         DataInputStream in;
+        OutputStream out;
 
         int [] lengthBytes = new int[2];
         int [] messageBytes = new int[2000];
@@ -51,6 +52,7 @@ public class MipConnection extends Thread {
                 isConnected = true;
                 System.out.println("MCBN connetced on port 11000");
                 in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+                out = socket.getOutputStream();
 
                 lengthBytes[0] = in.readUnsignedByte();
                 lengthBytes[1] = in.readUnsignedByte();
@@ -67,7 +69,23 @@ public class MipConnection extends Thread {
 
                 System.out.println(sb.toString()); 
 
-                onRequest(sb.toString());
+                String responseHexString = onRequest(sb.toString());
+
+                byte[] responseBytes = new byte[responseHexString.length() / 2];
+                byte[] responseLength = ByteBuffer.allocate(4).putInt(responseBytes.length).array();
+
+                for (byte b : responseLength) {
+                    System.out.format("==================>>>> 0x%x ", b);
+                 }
+
+                for (int i = 0; i < responseBytes.length; i++) {
+                    int index = i * 2;
+                    int val = Integer.parseInt(responseHexString.substring(index, index + 2), 16);
+                    responseBytes[i] = (byte)val;
+                }
+
+                out.write(responseLength,2,2);
+                out.write(responseBytes);
                 
             } catch (Exception e) {
                 System.out.println(String.format("unable to connect to MCBN. time=%s", LocalTime.now().toString()));
